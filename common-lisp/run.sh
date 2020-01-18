33088cdc7cb8b70e5fa79196b15bdc30c2acd410
gos-k <mag4.elan@gmail.com>
2020-01-18 23:20:24 +0900

    mod parallel

--- common-lisp/run.sh
+++ common-lisp/run.sh
@@ -1,1 +1,2 @@
-ros run -l collatz-producer-consumer.ros -e "(ros.script.collatz.3787876844::main)" -q -- --dynamic-space-size 30000
+#ros run -l collatz-producer-consumer.ros -e "(ros.script.collatz.3787876844::main)" -q -- --dynamic-space-size 30000
+ros run -l collatz.ros -e "(ros.script.collatz.3787876844::main)" -q -- --dynamic-space-size 30000
